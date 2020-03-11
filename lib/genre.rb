453f5ad9a4609061a8c896ebc7747d88bfddcae3
Michael Kwiatkowsky <mikeski885@gmail.com>
2020-03-11 01:39:23 +0000

    Done.

---
+++ lib/genre.rb
@@ -0,0 +1,19 @@
+
+class Genre
+
+  @@all = []
+
+  def self.all
+    @@all
+  end
+
+  attr_accessor :name, :artists, :songs
+
+  def initialize(name)
+    @artists = []
+    @songs = []
+    @name = name
+    @@all << self
+  end
+
+end
