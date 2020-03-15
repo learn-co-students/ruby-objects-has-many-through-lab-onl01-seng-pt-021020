58aebb40f1b7866259dc5432661e4398332c172d
Amber Williams <awilliams1234@live.com>
2020-03-15 23:21:15 +0000

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,23 +1,33 @@
-class Artist 
-  attr_accessor :name 
-  @@all = []
-  
+class Artist
+
+  attr_accessor :name, :artist, :genre
+
+  @@all =[]
+
   def initialize(name)
     @name = name
-    @@all << self 
-  end 
-  
+    @@all << self
+  end
+
   def self.all
     @@all
-  end 
-  
-  def song  
-  end 
-  
-  def new_song 
-  end 
-  
-  def genres 
-  end 
-  
+  end
+
+  def new_song(name, genre)
+    song = Song.new(name, self, genre)
+    self.songs.last
+  end
+
+  def songs
+    Song.all.select do |song|
+      song.artist == self
+    end
+  end
+
+  def genres
+    Song.all.map do |song|
+      song.genre
+    end
+  end
+
 end
--- lib/song.rb
+++ lib/song.rb
@@ -1,18 +1,22 @@
-class Song 
-  
-  attr_accessor :name, :artist, :genre 
-  
+class Song
+
+  attr_accessor :name, :artist, :genre
+
   @@all = []
-  
+
   def initialize(name, artist, genre)
     @name = name
     @artist = artist
     @genre = genre
     @@all << self
-  end 
-  
+  end
+
   def self.all
     @@all
   end
-  
-end 
+
+  def artist
+    @artist
+  end
+
+end
